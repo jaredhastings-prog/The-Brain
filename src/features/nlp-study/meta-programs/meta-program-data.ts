ecca8ddafc572bb307b4d39a345da8b6a4f19653
jaredhastings-prog <jared.hastings@outlook.com>
2026-05-25 11:39:37 +1000

    chore: use full meta-program deck data

--- src/features/nlp-study/meta-programs/meta-program-data.ts
+++ src/features/nlp-study/meta-programs/meta-program-data.ts
@@ -1,5 +1,5 @@
 import categoriesJson from "../../../../docs/docs/nlp/meta-program-categories.json";
-import cardsJson from "../../../../docs/docs/nlp/meta-program-cards-starter.json";
+import cardsJson from "../../../../docs/docs/nlp/meta-program-cards.json";
 import visualScenesJson from "../../../../docs/docs/nlp/visual-scene-prompts-starter.json";
 
 import type {
